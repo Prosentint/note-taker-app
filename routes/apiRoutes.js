0a7e3a2b9b64f99f14ae24b4cd0ee8ac083758a4
Blackberry <david2631@gmail.com>
2023-10-11 13:28:36 -0400

    Added delete functionality

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,9 +3,11 @@ const path = require('path');
 const router = require('express').Router();
 const uuid = require('uuid');
 
+const dbFilePath = path.join(__dirname, '../db/db.json');
+
 // Route to get all saved notes
 router.get('/notes', (req, res) => {
-  fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
+  fs.readFile(dbFilePath, 'utf8', (err, data) => {
     if (err) {
       console.error(err);
       return res.status(500).json({ error: 'Error reading notes.' });
@@ -19,17 +21,17 @@ router.get('/notes', (req, res) => {
 router.post('/notes', (req, res) => {
     const newNote = req.body;
   
-    fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
+    fs.readFile(dbFilePath, 'utf8', (err, data) => {
       if (err) {
         console.error(err);
         return res.status(500).json({ error: 'Error reading notes.' });
       }
   
       const notes = JSON.parse(data);
-      newNote.id = uuid(); // Generate a unique ID using uuid/v1
+      newNote.id = uuid(); 
       notes.push(newNote);
   
-      fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notes), (err) => {
+      fs.writeFile(dbFilePath, JSON.stringify(notes), (err) => {
         if (err) {
           console.error(err);
           return res.status(500).json({ error: 'Error writing notes.' });
@@ -38,5 +40,36 @@ router.post('/notes', (req, res) => {
       });
     });
   });
+
+// Route to delete a note
+router.delete('/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+  
+    fs.readFile(dbFilePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Error reading notes.' });
+      }
+  
+      const notes = JSON.parse(data);
+  
+      const noteIndex = notes.findIndex((note) => note.id === noteId);
+  
+      if (noteIndex === -1) {
+        return res.status(404).json({ error: 'Note not found' });
+      }
+  
+      notes.splice(noteIndex, 1);
+  
+      fs.writeFile(dbFilePath, JSON.stringify(notes), (err) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ error: 'Error writing notes.' });
+        }
+        res.json({ message: 'Note deleted' });
+      });
+    });
+  });
+
 
 module.exports = router;
